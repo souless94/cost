ff4310f0858c69c8c984d95f8c25d6e0d894c64a
souless <clwk94@gmail.com>
2020-03-29 21:06:02 +0800

    Modifications - change debug back to true - fix typo in allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1','nameofapp.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1','costanalyzerxd.herokuapp.com']
 
 
 # Application definition
