c46f39f7056464d57932c11f61808e5c0c5b36e5
souless <clwk94@gmail.com>
2020-04-10 11:18:39 +0800

    Modifications - align chart to middle

--- static/main-chart.js
+++ static/main-chart.js
@@ -39,10 +39,9 @@ $(document).ready(function () {
             data: ['expenses', 'expenses_total']
         },
         grid: {
-            left: '0',
-            right: '0',
-            bottom: '0',
-			top: '0',
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
             containLabel: true
         },
         toolbox: {
